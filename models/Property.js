b979cfc67d690c43a861c3db30d0b1f987ebcbd3
Moosa <moosajamel.21@gmail.com>
2025-01-15 11:31:02 +0300

    Models Done

---
+++ models/Property.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose')
+
+const propertySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  location: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  amenities: [
+    {
+      type: String,
+      required: false
+    }
+  ],
+  availability: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  discount: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100
+  },
+  discountedPrice: {
+    type: Number,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = propertySchema
